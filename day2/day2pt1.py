6542d9972430a585aa148271a23c6af35abddef8
acpirience <acpirience@gmail.com>
2022-12-19 14:30:42 +0100

    Day 2

---
+++ day2/day2pt1.py
@@ -0,0 +1,49 @@
+from loguru import logger
+
+
+def main():
+    """
+    https://adventofcode.com/2022/day/1
+    """
+    day = "2"
+    part = "1"
+
+    logger.info(f"Day {day} part {part}")
+    # fileInputName = "day" + day + "exampleinput.txt"
+    fileInputName = "day" + day + "input.txt"
+
+    # load input
+    result = ""
+    logger.info(f"Working with file: {fileInputName}")
+
+    # Opponent
+    # A, B, C => Rock, Paper, Scissors
+
+    # me
+    # X = Rock = 1; Y = Paper = 2, Z = Scissors = 3
+
+    playValues = {"X": 1, "Y": 2, "Z": 3}
+    winLosses = {
+        "AX": 3,
+        "AY": 6,
+        "AZ": 0,
+        "BX": 0,
+        "BY": 3,
+        "BZ": 6,
+        "CX": 6,
+        "CY": 0,
+        "CZ": 3,
+    }
+
+    result = 0
+
+    with open(fileInputName, "r") as inputFile:
+        for line in inputFile.readlines():
+            line = line.strip().replace(" ", "")
+            result += winLosses[line] + playValues[line[1]]
+
+    logger.info(f"Day {day} part {part} result: {result}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day2/day2pt2.py
@@ -0,0 +1,52 @@
+from loguru import logger
+
+# Opponent
+# A, B, C => Rock, Paper, Scissors
+
+# me
+# Rock = 1; Paper = 2, Scissors = 3
+# X => need to lose (0), Y => need to draw (3), Z => need to win (6)
+playValues = {"A": 1, "B": 2, "C": 3}
+outcomeValues = {"X": 0, "Y": 3, "Z": 6}
+winPlay = {"A": "B", "B": "C", "C": "A"}
+loosePlay = {"A": "C", "B": "A", "C": "B"}
+
+
+def calcPlay(opponent, outcome):
+    match outcome:
+        case "Y":
+            return playValues[opponent]
+        case "X":
+            return playValues[loosePlay[opponent]]
+        case "Z":
+            return playValues[winPlay[opponent]]
+
+
+def main():
+    """
+    https://adventofcode.com/2022/day/1
+    """
+    day = "2"
+    part = "2"
+
+    logger.info(f"Day {day} part {part}")
+    # fileInputName = "day" + day + "exampleinput.txt"
+    fileInputName = "day" + day + "input.txt"
+
+    # load input
+    result = ""
+    logger.info(f"Working with file: {fileInputName}")
+
+    result = 0
+
+    with open(fileInputName, "r") as inputFile:
+        for line in inputFile.readlines():
+            line = line.strip().replace(" ", "")
+            opponent, outcome = line[0], line[1]
+            result += outcomeValues[outcome] + calcPlay(opponent, outcome)
+
+    logger.info(f"Day {day} part {part} result: {result}")
+
+
+if __name__ == "__main__":
+    main()
