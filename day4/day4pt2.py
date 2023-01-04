292b5cbf879339e1183679646c627f24d1e51d82
acpirience <acpirience@gmail.com>
2023-01-04 11:50:50 +0100

    day 4

---
+++ day4/day4pt2.py
@@ -0,0 +1,40 @@
+from loguru import logger
+
+
+def section_overlap_section(left, right):
+    if (right[0] <= left[0] <= right[1]) or (right[0] <= left[1] <= right[1]):
+        return True
+    if (left[0] <= right[0] <= left[1]) or (left[0] <= right[1] <= left[1]):
+        return True
+    return False
+
+
+def main():
+    """
+    https://adventofcode.com/2022/day/4
+    """
+    day = "4"
+    part = "2"
+
+    logger.info(f"Day {day} part {part}")
+    # fileInputName = "day" + day + "exampleinput.txt"
+    fileInputName = "day" + day + "input.txt"
+
+    # load input
+    logger.info(f"Working with file: {fileInputName}")
+
+    result = 0
+
+    with open(fileInputName, "r") as inputFile:
+        for line in inputFile.readlines():
+            left, right = line.strip().split(",")
+            left = [int(x) for x in left.split("-")]
+            right = [int(x) for x in right.split("-")]
+            if section_overlap_section(left, right):
+                result += 1
+
+    logger.info(f"Day {day} part {part} result: {result}")
+
+
+if __name__ == "__main__":
+    main()
